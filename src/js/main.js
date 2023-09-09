d2b82209b5d54593db8dfec46c2b38601fa820f2
ahmadadam12 <adampython5@gmail.com>
2023-09-09 17:11:37 +0700

    major update

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,45 @@
 import Typed from 'typed.js';
-import confetti from 'canvas-confetti';
+// import confetti from 'canvas-confetti';
 
 const typed = new Typed('#heading', {
-  strings: ["I am product designer", "Software Engineer", "and A cat"],
+  strings: ["Modern Web Development"],
   typeSpeed: 50,
   backSpeed: 98,
   cursorChar:" ",
   loop: true,
 });
 
-const form = document.getElementById("form");
-form.addEventListener("keydown",(e)=>{
-    if(e.key==="Enter"){
-        const myCanvas = document.createElement('canvas');
-        document.body.appendChild(myCanvas);
-        confetti.create(myCanvas, {
-            resize: true,
-            useWorker: true
-          });
-        confetti({
-            particleCount: 1000,
-            spread: 160,
-          });
-          myCanvas.remove();
-    }
-});
+const typed2 = new Typed('#heading2', {
+    strings: ["Data Science"],
+    typeSpeed: 50,
+    backSpeed: 98,
+    cursorChar:" ",
+    loop: true,
+  });
+
+const typed3 = new Typed('#heading3', {
+    strings: ["Product Design"],
+    typeSpeed: 50,
+    backSpeed: 98,
+    cursorChar:" ",
+    loop: true,
+  });
+
+
+
+// const form = document.getElementById("form");
+// form.addEventListener("keydown",(e)=>{
+//     if(e.key==="Enter"){
+//         const myCanvas = document.createElement('canvas');
+//         document.body.appendChild(myCanvas);
+//         confetti.create(myCanvas, {
+//             resize: true,
+//             useWorker: true
+//           });
+//         confetti({
+//             particleCount: 1000,
+//             spread: 160,
+//           });
+//           myCanvas.remove();
+//     }
+// });
